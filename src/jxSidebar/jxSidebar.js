1c18feacb4bd38b80885a1bda68db458e65ad7e7
bandras73 <fpkbandras@freemail.hu>
2023-01-06 20:03:48 +0100

    create JXSidebar

---
+++ src/jxSidebar/jxSidebar.js
@@ -0,0 +1,123 @@
+import React from "react";
+import ListSubheader from '@mui/material/ListSubheader';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import SendIcon from '@mui/icons-material/Send';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import StarBorder from '@mui/icons-material/StarBorder';
+
+
+
+import './jxSidebar.scss';
+
+function SidebarItem({ depthStep = 10, depth = 0, expanded, item, ...rest }) {
+  const [collapsed, setCollapsed] = React.useState(true);
+  const { label, items, Icon, onClick: onClickProp } = item;
+
+  function toggleCollapse() {
+    setCollapsed(prevValue => !prevValue);
+  }
+
+  function onClick(e) {
+    if (Array.isArray(items)) {
+      toggleCollapse();
+    }
+    if (onClickProp) {
+      onClickProp(e, item);
+    }
+  }
+
+  let expandIcon;
+
+  if (Array.isArray(items) && items.length) {
+    expandIcon = !collapsed ? (
+      <ExpandLessIcon
+        className={
+          "sidebar-item-expand-arrow" + " sidebar-item-expand-arrow-expanded"
+        }
+      />
+    ) : (
+      <ExpandMoreIcon className="sidebar-item-expand-arrow" />
+    );
+  }
+
+  return (
+    <>
+      <ListItemButton
+        className="sidebar-item"
+        onClick={onClick}
+        button
+        dense
+        {...rest}
+      >
+        <div
+          style={{ paddingLeft: depth * depthStep }}
+          className="sidebar-item-content"
+        >
+          {Icon && <ListItemIcon> <Icon className="sidebar-item-icon" fontSize="medium" /> </ListItemIcon>}
+          <div className="sidebar-item-text"><ListItemText primary={label} /></div>
+        </div>
+
+        {expandIcon}
+      </ListItemButton>
+
+      <Collapse in={!collapsed} timeout="auto" unmountOnExit>
+        {Array.isArray(items) ? (
+          <List disablePadding dense>
+            {items.map((subItem, index) => (
+              <React.Fragment key={`${subItem.name}${index}`}>
+                {subItem === "divider" ? (
+                  <Divider style={{ margin: "6px 0" }} />
+                ) : (
+                  <SidebarItem
+                    depth={depth + 1}
+                    depthStep={depthStep}
+                    item={subItem}
+                  />
+                )}
+              </React.Fragment>
+            ))}
+          </List>
+        ) : null}
+      </Collapse>
+    </>
+  );
+}
+
+function JXSidebar({ items, depthStep, depth, expanded }) {
+  return (
+    <div className="sidebar">
+      <List
+        component="nav"
+        disablePadding
+        dense>
+
+        {items.map((sidebarItem, index) => (
+          <React.Fragment key={`${sidebarItem.name}${index}`}>
+            {sidebarItem === "divider" ? (
+              <Divider style={{ margin: "6px 0" }} />
+            ) : (
+              <SidebarItem
+                depthStep={depthStep}
+                depth={depth}
+                expanded={expanded}
+                item={sidebarItem}
+              />
+            )}
+          </React.Fragment>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+
+export default JXSidebar;
